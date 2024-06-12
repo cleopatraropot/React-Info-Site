ba9a6de3d6fb944dee1179960af6a19842875065
cleopatraropot <cleopatraropot@gmail.com>
2024-06-12 16:46:52 +0300

    Lay the base of the site(the most simple way without styling)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom/client';
+
+const page = (
+    <div>
+      <img src="/main-logo.png" style={{width: '70px'}} alt="React Logo."/>
+      <h1>Fun fact about react</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created in Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+)
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(page)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="root">
+      </div>
   );
 }
 
